511bcd000b66f8b79300217ace20b93a7f9b1889
McEnigma24 <116126603+McEnigma24@users.noreply.github.com>
2023-08-20 20:05:51 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include "headers.h"
+
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    //Proste_Wyswietlanie(tab);
+
+    //Wyswietl_Plansze(tab, feed_back);
+
+    Gra();
+
+    return 0;
+}
